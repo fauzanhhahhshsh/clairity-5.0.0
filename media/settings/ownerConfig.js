f1c719c669585d235b0957041ef3c9d71e1c8236
fauzanhhahhshsh <fauzannibosk@gmail.com>
2025-02-09 18:12:29 +0800

    Update ownerConfig.js
    New Owner

--- media/settings/ownerConfig.js
+++ media/settings/ownerConfig.js
@@ -2,8 +2,8 @@
 const fs = require('fs')
 // gausah di ubah rek karna
 // udh ada request pas di scan pairing
-global.ownername = 'xZiyy';
-global.owner = '6283804078729';
+global.ownername = 'F';
+global.owner = '6282189212851';
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
